7dea17280ad5bd9c121e7a3384eb40169658a530
John Haste <joaovictorspteixeira@gmail.com>
2022-06-10 00:36:23 -0300

    Updated Gun Position When Grabbing

--- Assets/GrabObjectsDetector.cs
+++ Assets/GrabObjectsDetector.cs
@@ -6,13 +6,17 @@ public class GrabObjectsDetector : MonoBehaviour
 {
 
     public GameObject rightHand;
+    private bool isGrabbingGun;
 
     void OnTriggerEnter(Collider col)
     {
 
-        if(col.name == "GunGrab")
+        if(col.name == "GunGrab" && !isGrabbingGun)
         {
-            col.transform.position = rightHand.transform.position + new Vector3(0f, 0.9f, 0.3f);
+            isGrabbingGun = true;
+
+            col.transform.position = rightHand.transform.position + new Vector3(0f, 0f, 0.3f);
+            col.transform.rotation = Quaternion.Euler(rightHand.transform.eulerAngles.x,rightHand.transform.eulerAngles.y,rightHand.transform.eulerAngles.z);
             col.transform.parent = rightHand.transform;
 
             //Get the gun properties
