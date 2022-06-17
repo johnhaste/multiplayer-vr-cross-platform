7389617724d671aca6a2b927b91d0bb63aac1cd5
John Haste <joaovictorspteixeira@gmail.com>
2022-06-16 21:24:48 -0300

    Guns with right physics and not bouncing

--- Assets/ObjectGrabber.cs
+++ Assets/ObjectGrabber.cs
@@ -21,11 +21,13 @@ public class ObjectGrabber : MonoBehaviour
             //Adjusting position and parenting
             col.transform.position = rightHand.transform.position + new Vector3(0f, -0.2f, 0.3f);
             col.transform.rotation = Quaternion.Euler(rightHand.transform.eulerAngles.x,rightHand.transform.eulerAngles.y,rightHand.transform.eulerAngles.z);
-            col.transform.parent   = rightHand.transform;
+            col.transform.SetParent(rightHand.transform);
 
             //Get the gun properties
-            col.gameObject.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-        
+            //col.gameObject.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+            col.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            col.gameObject.GetComponent<BoxCollider>().enabled = false;
 
             if(!CurrentPlatformManager.instance.IsOnQuest())
             {
