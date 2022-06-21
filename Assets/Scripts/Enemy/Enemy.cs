1846c40e1f6ce0750fa96c0b53a922f01df205c8
John Haste <joaovictorspteixeira@gmail.com>
2022-06-20 23:54:41 -0300

    New Scripts Folder Organization

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -116,7 +116,7 @@ public class Enemy : MonoBehaviour, IPunObservable
 
     IEnumerator WaitAndDie()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         Destroy(gameObject);
     }
 
